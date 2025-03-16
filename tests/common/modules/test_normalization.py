b001d817c2319afe7090a88f1ad8b7d350d14b39
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2025-03-15 22:29:11 -0500

    add Dynamic Tanh

--- tests/common/modules/test_normalization.py
+++ tests/common/modules/test_normalization.py
@@ -1,14 +1,14 @@
 import pytest
 import torch
 
-from wiskers.common.modules.normalization import AdaIN
+from wiskers.common.modules.normalization import AdaIN, DyT
 
 
 @pytest.mark.parametrize(
     "batch_size, in_channels, height, width",
     [
-        (4, 3, 128, 128),
-        (8, 1, 256, 256),
+        (4, 3, 64, 64),
+        (8, 1, 32, 32),
     ],
 )
 def test_AdaIN(batch_size, in_channels, height, width):
@@ -17,6 +17,46 @@ def test_AdaIN(batch_size, in_channels, height, width):
     y = torch.randn(batch_size, in_channels, height, width)
 
     out_x = net(x, y)
+    assert isinstance(out_x, torch.Tensor)
+    assert out_x.shape == x.shape
+    assert out_x.dtype == x.dtype
+
+
+@pytest.mark.parametrize(
+    "batch_size, in_channels, height, width",
+    [
+        (4, 3, 64, 64),
+        (8, 1, 32, 32),
+    ],
+)
+def test_DyT_element_wise(batch_size, in_channels, height, width):
+    net = DyT(normalized_shape=(in_channels, height, width), channels_last=True)
+
+    x = torch.randn(batch_size, in_channels, height, width)
+
+    out_x = net(x)
+
+    # Assertions
+    assert isinstance(out_x, torch.Tensor)
+    assert out_x.shape == x.shape
+    assert out_x.dtype == x.dtype
+
+
+@pytest.mark.parametrize(
+    "batch_size, in_channels, height, width",
+    [
+        (4, 3, 64, 64),
+        (8, 1, 32, 32),
+    ],
+)
+def test_DyT_channel_wise(batch_size, in_channels, height, width):
+    net = DyT(normalized_shape=in_channels, channels_last=False)
+
+    x = torch.randn(batch_size, in_channels, height, width)
+
+    out_x = net(x)
+
+    # Assertions
     assert isinstance(out_x, torch.Tensor)
     assert out_x.shape == x.shape
     assert out_x.dtype == x.dtype
--- wiskers/common/modules/normalization.py
+++ wiskers/common/modules/normalization.py
@@ -31,3 +31,40 @@ class AdaIN(nn.Module):
         stylized_x = normalized_x * torch.sqrt(y_var + self.EPSILON) + y_mean
 
         return stylized_x
+
+
+class DyT(nn.Module):
+    """
+    Dynamic Tanh (DyT)
+    Paper: Transformers without Normalization
+    GitHub(Implementation): https://jiachenzhu.github.io/DyT/
+
+    Shapes:
+        Input: (N, C) or (N, C, H, W) depending on `channels_last`
+        Output: (N, C) or (N, C, H, W)
+
+    Args:
+        normalized_shape (int): The number of features (C) to normalize.
+        channels_last (bool): If True, applies element-wise transformation.
+                              If False, applies channel-wise transformation for spatial inputs.
+        alpha_init_value (float): Initial value for alpha parameter.
+
+    """
+
+    def __init__(self, normalized_shape, channels_last, alpha_init_value=0.5):
+        super().__init__()
+        self.normalized_shape = normalized_shape
+        self.alpha_init_value = alpha_init_value
+        self.channels_last = channels_last
+
+        self.alpha = nn.Parameter(torch.ones(1) * alpha_init_value)
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+
+    def forward(self, x):
+        x = torch.tanh(self.alpha * x)
+        if self.channels_last:
+            x = x * self.weight + self.bias  # Element-wise transformation
+        else:
+            x = x * self.weight[:, None, None] + self.bias[:, None, None]  # Broadcast across spatial dims
+        return x
