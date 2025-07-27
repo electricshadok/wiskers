5dce358e14744efab6e358513b43046851865348
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2025-07-26 23:36:08 -0500

    update clevrer

--- wiskers/datasets/clevrer.py
+++ wiskers/datasets/clevrer.py
@@ -1,17 +1,19 @@
 import os
+import zipfile
 from urllib.request import Request, urlopen, urlretrieve
 
 import lightning as L
 
 
 def get_file_size(url):
-    request = Request(url, method='HEAD')
+    request = Request(url, method="HEAD")
     with urlopen(request) as response:
-        size = response.getheader('Content-Length')
+        size = response.getheader("Content-Length")
         if size is not None:
             return int(size)
         else:
             return None
+
 
 class CLEVRER(L.LightningDataModule):
     """
@@ -26,20 +28,34 @@ class CLEVRER(L.LightningDataModule):
     """
 
     QA_URLS = {
-        "train": ("http://data.csail.mit.edu/clevrer/questions/train.json", "train.json"),
-        "valid": ("http://data.csail.mit.edu/clevrer/questions/validation.json", "validation.json"),
+        "train": (
+            "http://data.csail.mit.edu/clevrer/questions/train.json",
+            "train.json",
+        ),
+        "valid": (
+            "http://data.csail.mit.edu/clevrer/questions/validation.json",
+            "validation.json",
+        ),
         "test": ("http://data.csail.mit.edu/clevrer/questions/test.json", "test.json"),
     }
 
-    VIDEO_URLS = {"train": ("http://data.csail.mit.edu/clevrer/videos/train/video_train.zip",
-                            "video_train.zip"),
-                  "valid": ("http://data.csail.mit.edu/clevrer/videos/validation/video_validation.zip",
-                            "video_validation.zip"),
-                  "test": ("http://data.csail.mit.edu/clevrer/videos/test/video_test.zip",
-                           "video_test.zip"),
-                  }
+    VIDEO_URLS = {
+        "train": (
+            "http://data.csail.mit.edu/clevrer/videos/train/video_train.zip",
+            "video_train.zip",
+        ),
+        "valid": (
+            "http://data.csail.mit.edu/clevrer/videos/validation/video_validation.zip",
+            "video_validation.zip",
+        ),
+        "test": (
+            "http://data.csail.mit.edu/clevrer/videos/test/video_test.zip",
+            "video_test.zip",
+        ),
+    }
 
     def __init__(self, data_dir: str, batch_size: int, num_workers: int):
+        super().__init__()
         self.data_dir = os.path.join(data_dir, "clevrer")
         self.batch_size = batch_size
         self.num_workers = num_workers
@@ -56,14 +72,16 @@ class CLEVRER(L.LightningDataModule):
                 local_path = os.path.join(qa_dir, local_path)
                 if not os.path.exists(local_path):
                     size_mb = get_file_size(url_path) / (1024 * 1024)
-                    print(f"Downloading CLEVRER Question-Answer ({setname}) of size {size_mb:.2f} MB to {local_path}...")
+                    print(
+                        f"Downloading CLEVRER Question-Answer ({setname}) of size {size_mb:.2f} MB to {local_path}..."
+                    )
                     urlretrieve(url_path, local_path)
                 else:
                     print(f"CLEVRER Question-Answer ({setname}) already downloaded.")
             else:
                 print(f"CLEVRER Question-Answer ({setname}) not specified...")
 
-        # Downlod Zip video
+        # Downlod Zip video and Unzip them
         video_dir = os.path.join(self.data_dir, "videos")
         os.makedirs(video_dir, exist_ok=True)
         for setname, url_n_local in self.VIDEO_URLS.items():
@@ -72,14 +90,20 @@ class CLEVRER(L.LightningDataModule):
                 local_path = os.path.join(video_dir, local_path)
                 if not os.path.exists(local_path):
                     size_mb = get_file_size(url_path) / (1024 * 1024)
-                    print(f"Downloading CLEVRER Video ({setname}) of size {size_mb:.2f} MB to {local_path}...")
+                    print(
+                        f"Downloading CLEVRER Video ({setname}) of size {size_mb:.2f} MB to {local_path}..."
+                    )
                     urlretrieve(url_path, local_path)
                 else:
                     print(f"CLEVRER Video ({setname}) already downloaded.")
             else:
                 print(f"CLEVRER Video ({setname}) not specified...")
 
-        # Unzip Videos
-        # TODO
-        #  with ZipFile(zip_path, 'r') as zip_ref:
-        #        zip_ref.extractall(self.data_dir)
+            raw_video_dir = os.path.join(video_dir, setname)
+            if not os.path.exists(raw_video_dir):
+                os.makedirs(raw_video_dir, exist_ok=True)
+                with zipfile.ZipFile(local_path, "r") as zip_ref:
+                    print(f"CLEVRER Unzip Video ({setname}).")
+                    zip_ref.extractall(raw_video_dir)
+            else:
+                print(f"CLEVRER Video ({setname}) already unzipped.")
