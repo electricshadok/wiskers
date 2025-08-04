e11181710074921240d71a83fc49806d074b5859
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2025-08-03 22:18:06 -0500

    fix dataset

--- wiskers/datasets/clevrer_dataset.py
+++ wiskers/datasets/clevrer_dataset.py
@@ -20,25 +20,24 @@ class Clevrer(Dataset):
         self.data_dir = data_dir
         self.split = split
         self.chunk_len = chunk_len
+        self.cache_dir = os.path.join(self.data_dir, "cache")
+        self.video_root = os.path.join(self.data_dir, "videos")
+        self.qa_root = os.path.join(self.data_dir, "question_answer")
+        self.video_dir = os.path.join(self.video_root, self.split)
+        self.json_path = os.path.join(self.video_root, f"{self.split}.json")
 
     def prepare_data(self):
-        video_root = os.path.join(self.data_dir, "videos")
-        video_dir = os.path.join(video_root, self.split)
-        json_path = os.path.join(video_root, f"{self.split}.json")
-
-        if os.path.exists(json_path):
-            print(f"Skipping {self.split}, already processed: {json_path}")
+        if os.path.exists(self.json_path):
+            print(f"Skipping {self.split}, already processed: {self.json_path}")
             return
 
-        build_frame_count_json(video_dir, json_path)
+        build_frame_count_json(self.video_dir, self.json_path)
 
     def download_all(self):
         os.makedirs(self.data_dir, exist_ok=True)
 
         # Download JSON Question_Answer
-        qa_dir = os.path.join(self.data_dir, "question_answer")
-        download_qa(qa_dir, self.split)
+        download_qa(self.qa_root, self.split)
 
         # Downlod Zip video and Unzip them
-        video_dir = os.path.join(self.data_dir, "videos")
-        download_videos(video_dir, self.split)
+        download_videos(self.video_root, self.split)
