4cea8c72c20879b12536cacfa317535f068c6fbd
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2025-08-03 22:53:25 -0500

    add json

--- wiskers/datasets/clevrer.py
+++ wiskers/datasets/clevrer.py
@@ -1,6 +1,7 @@
 import os
 
 import lightning as L
+from torch.utils.data import DataLoader
 
 import wiskers.datasets.clevrer_dataset as data
 
@@ -20,18 +21,45 @@ class CLEVRER(L.LightningDataModule):
     def __init__(self, data_dir: str, batch_size: int, num_workers: int):
         super().__init__()
         self.data_dir = os.path.join(data_dir, "clevrer")
-        self.train = data.Clevrer(self.data_dir, "train", 4)
-        self.test = data.Clevrer(self.data_dir, "test", 4)
-        self.valid = data.Clevrer(self.data_dir, "valid", 4)
+        self.train_dataset = data.Clevrer(self.data_dir, "train", 4)
+        self.test_dataset = data.Clevrer(self.data_dir, "test", 4)
+        self.val_dataset = data.Clevrer(self.data_dir, "valid", 4)
         self.batch_size = batch_size
         self.num_workers = num_workers
 
     def prepare_data(self):
-        self.train.download_all()
-        self.train.prepare_data()
+        self.train_dataset.download_all()
+        self.train_dataset.prepare_data()
 
-        self.test.download_all()
-        self.test.prepare_data()
+        self.test_dataset.download_all()
+        self.test_dataset.prepare_data()
 
-        self.valid.download_all()
-        self.valid.prepare_data()
+        self.val_dataset.download_all()
+        self.val_dataset.prepare_data()
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            persistent_workers=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            persistent_workers=True,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            persistent_workers=True,
+        )
--- wiskers/datasets/clevrer_dataset.py
+++ wiskers/datasets/clevrer_dataset.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from torch.utils.data import Dataset
@@ -20,18 +21,41 @@ class Clevrer(Dataset):
         self.data_dir = data_dir
         self.split = split
         self.chunk_len = chunk_len
-        self.cache_dir = os.path.join(self.data_dir, "cache")
+        self.cache_dir = os.path.join(self.data_dir, "cache", self.split)
         self.video_root = os.path.join(self.data_dir, "videos")
         self.qa_root = os.path.join(self.data_dir, "question_answer")
         self.video_dir = os.path.join(self.video_root, self.split)
         self.json_path = os.path.join(self.video_root, f"{self.split}.json")
 
-    def prepare_data(self):
-        if os.path.exists(self.json_path):
-            print(f"Skipping {self.split}, already processed: {self.json_path}")
-            return
+        # Store list of relative video paths
+        self.video_list = []
 
-        build_frame_count_json(self.video_dir, self.json_path)
+        # Build index: (video_idx, start_frame)
+        self.index = []
+
+    def __len__(self):
+        return len(self.index)
+
+    def prepare_data(self):
+        # Create cached json
+        if not os.path.exists(self.json_path):
+            build_frame_count_json(self.video_dir, self.json_path)
+        else:
+            print(f"Skipping {self.split}, already processed: {self.json_path}")
+
+        # Prepare indices for samples
+        with open(self.json_path, "r") as f:
+            frame_counts = json.load(f)
+
+        self.video_list = list(frame_counts.keys())
+        self.index = []
+        for video_idx, rel_path in enumerate(self.video_list):
+            num_frames = frame_counts[rel_path]
+            num_chunks = num_frames // self.chunk_len
+            for i in range(num_chunks):
+                self.index.append((video_idx, i * self.chunk_len))
+
+        print(f"CLEVRER {len(self.index)} samples loaded from {self.json_path}")
 
     def download_all(self):
         os.makedirs(self.data_dir, exist_ok=True)
